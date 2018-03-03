263b5f202ce18e5ecd92e38780ed6ad3c1d39916
Pascal Ulor <pascalulor@yahoo.com>
2018-03-03 15:42:47 +0100

    setup server and test get route. create dummy data to test post route

--- server/app.js
+++ server/app.js
@@ -5,4 +5,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Home page route
+app.get('/', (req, res) => {
+    res.status(200);
+    res.json({
+        name: 'Don Ulor',
+        message: 'Welcome to WeConnect'
+    });
+});
+
 app.listen(3000, () => console.log(`Application started on port ${port}`));
+export default app;
---
+++ server/dataModel/data.js
@@ -0,0 +1,13 @@
+const clientData = [
+    {
+        id: 1,
+        userName: 'Pascal',
+        password: '',
+        businessName: 'SlimTrader',
+        email: 'slimtrader@gmail.com',
+        category: 'IT',
+        Address: '123 V.I Lagos',
+        state: 'Lagos',
+        city: 'Island'
+    }
+]
