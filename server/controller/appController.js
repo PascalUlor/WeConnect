d548f3e8a85007e1c358a4ca18ab01bcedb507f8
Pascal Ulor <pascalulor@yahoo.com>
2018-03-03 15:57:38 +0100

    setup files for controller and dummy data

---
+++ server/controller/appController.js
@@ -0,0 +1,1 @@
+import clientData from '../dataModel/data';
--- server/dataModel/data.js
+++ server/dataModel/data.js
@@ -9,5 +9,17 @@ const clientData = [
         Address: '123 V.I Lagos',
         state: 'Lagos',
         city: 'Island'
+    },
+    {
+        id: 2,
+        userName: 'Emeka',
+        password: '',
+        businessName: 'interswitch',
+        email: 'interswitch@gmail.com',
+        category: 'finance',
+        Address: '40 V.I Lagos',
+        state: 'Lagos',
+        city: 'Island'
     }
 ]
+export default clientData;
