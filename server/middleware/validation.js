84a641152c609106d75b16aab341a0b5be2d4715
Pascal Ulor <pascalulor@yahoo.com>
2018-03-08 04:49:48 +0100

    install validator for input validation. create validator.js in middleware directory

---
+++ server/middleware/validation.js
@@ -0,0 +1,16 @@
+import validator from 'validator';
+
+/**
+ * Validates all routes
+ * @class Validator
+ */
+export default class Validation {
+    /**
+     * Validates all business details
+     * @param {obj} req
+     * @param {obj} res
+     * @param {obj} next
+     * @returns {obj} Validation error messages or contents of req.body
+     */
+    
+}
