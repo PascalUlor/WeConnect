78c59f2108d29378675846b5d691a2906c94a405
Pascal Ulor <pascalulor@yahoo.com>
2018-03-09 01:43:08 +0100

    createtest.js to write tests for application

---
+++ server/specs/test.js
@@ -0,0 +1,70 @@
+/**
+ * Test for dummy data API endpoints
+ */
+import supertest from 'supertest';
+import chai from 'chai';
+import app from '../app';
+
+const { expect } = chai,
+    request = supertest(app);
+
+describe('All test cases for application', () => {
+    describe('Test case for loading application home page', () => {
+        it('should load application home page', (done) => {
+            request.get('/')
+                .set('Content-Type', 'application/json')
+                .expect(200)
+                .end((err, res) => {
+                    expect(res.body).deep.equal({
+                        name: 'Don Ulor',
+                        message: 'Welcome to WeConnect'
+                    });
+                    if (err) done(err);
+                    done();
+                });
+        });
+    });
+
+    describe('Test Case For Invalid Routes', () => {
+        it('Should return a message when an invalid route is accessed', (done) => {
+            request
+                .get('/api/v1/some-rubbish')
+                .set('Connection', 'keep alive')
+                .set('Content-Type', 'application/json')
+                .expect(404)
+                .end((err, res) => {
+                    expect(res.body).deep.equal({
+                        message: 'Invalid routes'
+                    });
+                    if (err) done(err);
+                    done();
+                });
+        });
+
+        it('should fail to get route', (done) => {
+            request.get('/api/v1')
+                .set('Contet-Type', 'application/json')
+                .expect(404)
+                .end((err, res) => {
+                    expect(res.body).deep.equal({
+                        message: 'Invalid routes'
+                    });
+                    if (err) done(err);
+                    done();
+                });
+        });
+
+        it('should return `404` page for all invalid routes', (done) => {
+            request.get('/more-recipes/recipes')
+                .set('Content-Type', 'application/json')
+                .expect(404)
+                .end((err, res) => {
+                    expect(res.body).deep.equal({
+                        message: 'Invalid routes'
+                    });
+                    if (err) done(err);
+                    done();
+                });
+        });
+    });
+});
